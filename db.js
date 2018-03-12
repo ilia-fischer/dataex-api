0e6062161257a03e554a72f7b98cc7d1ee843f35
Katrin Shechtman <katrin.shechtman@thomsonreuters.com>
2018-03-12 08:25:44 -0400

    adding mongodb docker setup

--- db.js
+++ db.js
@@ -1,2 +1,9 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/dataex');
+var connectionStr = process.env.MONGO_DB_CONNECTION_STR 
+if (!connectionStr) {
+    connectionStr = 'mongodb://127.0.0.1:27017/dataex';
+}
+
+console.log('Connection string: ' + connectionStr);
+
+mongoose.connect(connectionStr);
