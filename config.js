10e0229566bd44392500c5539e0d48ff8a248144
nigel.decosta <nigel.decosta@thomsonreuters.com>
2018-03-22 15:06:02 +0000

    Changed BC port to 3000

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   'secret': 'supersecret',
   'enable_classifier': true,
   'blockchain_api_host': '35.229.120.209',
-  'blockchain_api_port': 3001
+  'blockchain_api_port': 3000
 };
