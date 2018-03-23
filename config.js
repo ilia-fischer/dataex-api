a6ef6117ff61597bab4304f4deea9387a1fdc0f5
nigel.decosta <nigel.decosta@thomsonreuters.com>
2018-03-23 14:09:00 +0000

    Updated BC server host

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 module.exports = {
   'secret': 'supersecret',
   'enable_classifier': true,
-  'blockchain_api_host': '35.229.120.209',
+  'blockchain_api_host': '35.196.144.57',
   'blockchain_api_port': 3000
 };
