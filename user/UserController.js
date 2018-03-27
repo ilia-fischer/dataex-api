e91eb18aa9e524ac357c1a620f5eb2f1f2f02bbc
ilia.fischer <ilia.fischer@tr.com>
2018-03-27 12:16:49 -0400

    Ignore Blockchain data when the Balance is 0. Assuming that new users are initialized with 0 balance.

--- user/UserController.js
+++ user/UserController.js
@@ -49,13 +49,19 @@ router.get('/', VerifyToken('Administrator'), function (req, res) {
 
                 .then((result) => {
                     let r = JSON.parse(result);
-                    User.findOneAndUpdate({ email: req.query.email }, { $set: { balance: r.balance } }, { new: true }, function (err, _users) {
-                        res.status(200).send(_users);
-                    });
+                    if (r.balance !== "" && r.balance !== "0") {
+                        User.findOneAndUpdate({ email: req.query.email }, { $set: { balance: r.balance } }, { new: true }, function (err, _users) {
+                            res.status(200).send(_users);
+                        });
+                    } else {
+                        User.findOne({ email: req.query.email }, function (err, _users) {
+                            res.status(200).send(_users);
+                        });
+                    }
                 })
                 .catch((err) => {
                     console.log("USERS GET FAILED", err);
-                    User.findOneAndUpdate({ email: req.query.email }, { $set: { balance: '0' } }, { new: true }, function (err, _users) {
+                    User.findOne({ email: req.query.email }, function (err, _users) {
                         res.status(200).send(_users);
                     });
                 });
@@ -76,17 +82,27 @@ router.get('/', VerifyToken('Administrator'), function (req, res) {
 
                     .then((result) => {
                         let r = JSON.parse(result);
-                        User.findOneAndUpdate({ email: users[i].email }, { $set: { balance: r.balance } }, { new: true }, function (err, _users) {
-                            returned_users.push(_users);
+                        if (r.balance !== "" && r.balance !== "0") {
+                            User.findOneAndUpdate({ email: users[i].email }, { $set: { balance: r.balance } }, { new: true }, function (err, _users) {
+                                returned_users.push(_users);
 
-                            if (--count === 0) {
-                                res.status(200).send(returned_users);
-                            }
-                        });
+                                if (--count === 0) {
+                                    res.status(200).send(returned_users);
+                                }
+                            });
+                        } else {
+                            User.findOne({ email: users[i].email }, function (err, _users) {
+                                returned_users.push(_users);
+
+                                if (--count === 0) {
+                                    res.status(200).send(returned_users);
+                                }
+                            });
+                        }
                     })
                     .catch((err) => {
                         console.log("USERS GET FAILED", err);
-                        User.findOneAndUpdate({ email: users[i].email }, { $set: { balance: '0' } }, { new: true }, function (err, _users) {
+                        User.findOne({ email: users[i].email }, function (err, _users) {
                             returned_users.push(_users);
 
                             if (--count === 0) {
@@ -113,13 +129,19 @@ router.get('/:id', VerifyToken('Administrator'), function (req, res) {
 
             .then((result) => {
                 let r = JSON.parse(result);
-                User.findOneAndUpdate({ email: user.email }, { $set: { balance: r.balance } }, { new: true }, function (err, _user) {
-                    res.status(200).send(_user);
-                });
+                if (r.balance !== "" && r.balance !== "0") {
+                    User.findOneAndUpdate({ email: user.email }, { $set: { balance: r.balance } }, { new: true }, function (err, _user) {
+                        res.status(200).send(_user);
+                    });
+                } else {
+                    User.findOne({ email: user.email }, function (err, _user) {
+                        res.status(200).send(_user);
+                    });
+                }
             })
             .catch((err) => {
                 console.log("USER GET FAILED", err);
-                User.findOneAndUpdate({ email: user.email }, { $set: { balance: '0' } }, { new: true }, function (err, _user) {
+                User.findOne({ email: user.email }, function (err, _user) {
                     res.status(200).send(_user);
                 });
             });
